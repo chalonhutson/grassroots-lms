0de073db379a38317d89b3399bb77836c3ce1a4f
Chalon Hutson <chalonhutson@gmail.com>
2024-07-26 17:53:26 -0500

    Added basic css connection to dashboard.

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -2,6 +2,30 @@ import "./Dashboard.css"
 
 export default function Dashboard() {
     return (
-        <div>Dashboard</div>
+        <div className="main">
+            <div className="topSection">
+                <div className="dashboardContainer calendarContainer">
+                    calendar stuff
+                </div>
+                <div className="dashboardContainer plannedCourssContainer">
+                    planned courses
+                </div>
+                <div className="dashboardContainer notificationsContainer">
+                    notifications
+                </div>
+                <div className="dashboardContainer statsticsContainer">
+                    statistics
+                </div>
+                <div className="dashboardContainer newAvailableCoursesContainer">
+                    new available courses
+                </div>
+                <div className="dashboardContainer yourStatsVisualContainer">
+                    your stats visual
+                </div>
+            </div>
+            <div className="dashboardContainer achievementsContainer">
+                achievements
+            </div>
+        </div>
     )
 }
